822f7decd1ff93934605fbc6dc5d9b5d59241bf6
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-04-08 13:08:51 +0600

    Задание 1

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const addBtn = document.getElementById('add-item-btn');
+const container = document.getElementById('container');
+
+
+function onClick(event) {
+    event.preventDefault();
+    const div = document.createElement('div')
+    div.style.display = 'flex';
+    div.style.justifyContent = 'space-around'
+    div.style.margin = '10px'
+    const p = document.createElement('p')
+    p.innerText = `Контейнер создан`
+    div.append(p)
+    const input = document.createElement('input')
+    input.type = 'button'
+    input.value = 'Закрыть'
+    input.style.height = '25px'
+    div.append(input)  
+    container.append(div)
+    setTimeout(() => {
+        div.remove();
+    }, 5000)
+    input.addEventListener('click', () => {
+        div.remove();
+    });
+}
+
+addBtn.addEventListener('click', onClick)
+
